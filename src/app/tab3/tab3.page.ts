da41e6de84d84c22dcc9d18b1d0bbb16fcb6040f
unknown <josuesaenz885@gmail>
2023-02-13 09:18:55 -0500

    first commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['diseños.css']
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'INFORMACION MANITO',
+      buttons: ['OK'],
+      inputs: [
+        {
+          placeholder: 'Ingresa tu name',
+        },
+        {
+          placeholder: 'Apellido ñaño (max 8 caracteres)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Edad ',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
 
+    await alert.present();
+  }
 }
+
